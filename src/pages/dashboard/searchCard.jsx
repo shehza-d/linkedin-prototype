327789c4f5c0770941e7a1adb6b14654d0fb6dbd
Shehzad <shehzaddiqbal@gmail.com>
2023-01-18 13:54:35 +0500

    Progress

--- src/pages/dashboard/searchCard.jsx
+++ src/pages/dashboard/searchCard.jsx
@@ -1,4 +1,5 @@
 import styles from "../../styles/dashboard/searchCard.module.css";
+import star from "../../assets/dashboard/Star.svg";
 
 export default function SearchCard({
   heading,
@@ -11,14 +12,23 @@ export default function SearchCard({
   return (
     <div className={styles.card}>
       <div className={styles.content}>
-        <div className={styles.cardHeader}><img src={logo} alt="" />
-        <div>
+        <div className={styles.cardHeader}>
+          <img src={logo} alt="" />
+          <div>
             <p>{heading}</p>
             <p>electronics</p>
-        </div>
+          </div>
+          <div>
+            <img src={star} alt="" />
+            <img src={star} alt="" />
+            <img src={star} alt="" />
+          </div>
         </div>
         <p>{text}</p>
-        <div className={styles.cardFooter}></div>
+        <div className={styles.cardFooter}>
+          <span>{number}</span>
+          <span>{address}</span>
+        </div>
       </div>
       <div className={styles.img}>
         <img src={img} alt="" />
