4b9655da8b92468bb73cac4f835bf0a774c9ffb5
Shehzad <shehzaddiqbal@gmail.com>
2023-01-23 11:40:58 +0500

    Firebase start

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+import { getAuth } from "firebase/auth";
+//import { getAuth } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js'
+import { getFirestore } from "firebase/firestore";
+//import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVZnu1Dgishw2o2eu_W0Al8tv25CTEYQI",
+  authDomain: "expense-tracker-4989b.firebaseapp.com",
+  projectId: "expense-tracker-4989b",
+  storageBucket: "expense-tracker-4989b.appspot.com",
+  messagingSenderId: "17893173019",
+  appId: "1:17893173019:web:29b3908b3ec4f99c4a15be",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+console.log(app);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
